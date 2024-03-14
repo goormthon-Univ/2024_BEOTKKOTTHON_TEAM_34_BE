96960354937c303759f34f410352f3c2ec12e948
simjaeyoun <spqjfgogo@gmail.com>
2024-03-14 10:16:55 +0900

    chore : 초기 프로젝트 생성

---
+++ Kkri/src/main/java/com/groom/Kkri/KkriApplication.java
@@ -0,0 +1,13 @@
+package com.groom.Kkri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkriApplication.class, args);
+	}
+
+}
---
+++ Kkri/src/test/java/com/groom/Kkri/KkriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groom.Kkri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
