b56995cc98f5c73b24aa2af6a7861cfd71297986
hoyeon <aviationlovers@naver.com>
2024-03-16 23:14:44 +0900

    feat: Dto 추가

---
+++ Kkri/src/main/java/com/groom/Kkri/domain/MemberDTO.java
@@ -0,0 +1,42 @@
+package com.groom.Kkri.domain;
+
+import com.groom.Kkri.entity.Member;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MemberDTO {
+
+    private Long id;
+    private String username;
+    private String nickname;
+    private String univ;
+    private Long point;
+    private Long consumePoint;
+    private Long earnPoint;
+
+    public static MemberDTO from(Member member) {
+        return new MemberDTO(
+                member.getId(),
+                member.getUsername(),
+                member.getNickname(),
+                member.getUniv(),
+                member.getPoint(),
+                member.getConsumePoint(),
+                member.getEarnPoint()
+        );
+    }
+
+    public Member toEntity() {
+        return Member.builder()
+                .id(this.id)
+                .username(this.username)
+                .nickname(this.nickname)
+                .univ(this.univ)
+                .point(this.point)
+                .build();
+    }
+}
