7b09a07be3fcd4d8a5bdd953863e51622619de02
hoyeon <aviationlovers@naver.com>
2024-03-20 01:08:29 +0900

    feat : BoardService 생성 및 작성

---
+++ Kkri/src/main/java/com/groom/Kkri/service/BoardService.java
@@ -0,0 +1,59 @@
+package com.groom.Kkri.service;
+
+import com.groom.Kkri.enums.State;
+import org.springframework.stereotype.Component;
+
+import com.groom.Kkri.entity.Board;
+import com.groom.Kkri.enums.Type;
+import com.groom.Kkri.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    @Autowired
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    public List<Board> getAllPosts() {
+        return boardRepository.findAll();
+    }
+
+    public List<Board> getPostsByType(Type type) {
+        return boardRepository.findByType(type);
+    }
+
+    public Board createPost(Board board) {
+        return boardRepository.save(board);
+    }
+
+    public Optional<Board> getPostById(Long boardId) {
+        return boardRepository.findById(boardId);
+    }
+
+    public Board updatePost(Board board) {
+        return boardRepository.save(board);
+    }
+
+    public void deletePost(Long boardId) {
+        boardRepository.deleteById(boardId);
+    }
+
+    public void changePostStatus(Long boardId, State newState) {
+        Optional<Board> optionalBoard = boardRepository.findById(boardId);
+        if (optionalBoard.isPresent()) {
+            Board board = optionalBoard.get();
+            board.updateState(newState);
+            boardRepository.save(board);
+        } else {
+            throw new IllegalArgumentException("Board with ID " + boardId + " does not exist.");
+        }
+    }
+}
