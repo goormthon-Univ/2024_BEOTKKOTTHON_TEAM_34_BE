804d1b238f806e270892c5150a19cfc32820f736
simjaeyoun <spqjfgogo@gmail.com>
2024-03-23 01:31:52 +0900

    refactor : 반환 변경

--- Kkri/src/main/java/com/groom/Kkri/controller/AttachmentController.java
+++ Kkri/src/main/java/com/groom/Kkri/controller/AttachmentController.java
@@ -1,8 +1,6 @@
 package com.groom.Kkri.controller;
 
 import com.groom.Kkri.dto.attach.AttachmentOutputDto;
-import com.groom.Kkri.dto.attach.AttachmentUpdateDto;
-import com.groom.Kkri.repository.AttachmentRepository;
 import com.groom.Kkri.service.AttachmentService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -11,7 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -28,7 +25,7 @@ public class AttachmentController {
 
     @GetMapping
     @Operation(summary = "게시판에 관련된 이미지를 가져오는 api", description = "")
-    public ResponseEntity<List<String>> getAttachment(@RequestParam("boardId") Long boardId){
+    public ResponseEntity<List<AttachmentOutputDto>> getAttachment(@RequestParam("boardId") Long boardId){
         return ResponseEntity.ok(attachmentService.getImages(boardId));
     }
 
