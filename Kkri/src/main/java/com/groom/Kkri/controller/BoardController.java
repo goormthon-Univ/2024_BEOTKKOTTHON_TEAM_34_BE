ef2fb66cfac784acb1aa8ebbab7075ba476c503f
hoyeon <aviationlovers@naver.com>
2024-03-22 18:33:33 +0900

    feat : boardController 수정

--- Kkri/src/main/java/com/groom/Kkri/controller/BoardController.java
+++ Kkri/src/main/java/com/groom/Kkri/controller/BoardController.java
@@ -26,54 +26,49 @@ public class BoardController {
 
     private final BoardService boardService;
 
-
-    @Operation(summary = "게시판을 가져오는 api",description = "")
+    @Operation(summary = "게시판을 불러오는 api", description = "")
     @GetMapping("/{type}")
     public ResponseEntity<BoardPageDto> getBoard(@PathVariable("type") Type type, @PageableDefault(size = 5, sort = "createdDate",
             direction = Sort.Direction.DESC)Pageable pageable)
     {
         return ResponseEntity.ok(boardService.getBoard(type,pageable));
     }
-
-    // GET endpoint to retrieve the list of posts with optional search parameters
-    @GetMapping
-    public ResponseEntity<List<Board>> getPostList() {
-        List<Board> postList = boardService.getAllPosts();
-        return ResponseEntity.ok(postList);
-    }
-
-    // GET endpoint to view the list of posts by bulletin board type
-//    @GetMapping("/{type}")
-//    public ResponseEntity<List<Board>> getPostListByType(@PathVariable Type type) {
-//        List<Board> postList = boardService.getPostsByType(type);
-//        return ResponseEntity.ok(postList);
-//    }
-
-    // POST endpoint to write a new post
+    @Operation(summary = "게시글 작성 api", description = "")
     @PostMapping("/{type}")
     public ResponseEntity<Board> writePost(@PathVariable Type type, @RequestBody Board board) {
         Board newPost = boardService.createPost(type, board);
         return ResponseEntity.status(HttpStatus.CREATED).body(newPost);
     }
 
-    // PATCH endpoint to edit a post
+    @Operation(summary = "게시글 수정 api", description = "")
     @PutMapping("/{type}/boardno/{boardId}")
-    public ResponseEntity<Board> editPost(@PathVariable Type type, @PathVariable Long boardId, @RequestBody Board updatedBoard) {
+    public ResponseEntity<Board> editPost(
+            @PathVariable Type type,
+            @PathVariable Long boardId,
+            @RequestBody Board updatedBoard)
+    {
         Board editedPost = boardService.updatePost(type, boardId, updatedBoard);
         return ResponseEntity.ok(editedPost);
     }
 
-    // DELETE endpoint to delete a post
+    @Operation(summary = "게시글 삭제 api", description = "")
     @DeleteMapping("/{type}/boardno/{boardId}")
-    public ResponseEntity<Void> deletePost(@PathVariable Type type, @PathVariable Long boardId) {
+    public ResponseEntity<Board> deletePost(
+            @PathVariable Type type,
+            @PathVariable Long boardId)
+    {
         boardService.deletePost(type, boardId);
         return ResponseEntity.noContent().build();
     }
 
-    // GET endpoint to change transaction status
+    @Operation(summary = "거래 상태 변경 api", description = "")
     @GetMapping("/{boardId}/status")
-    public ResponseEntity<String> changeTransactionStatus(@PathVariable Long boardId, @RequestParam State newState) {
+    public ResponseEntity<Board> changeTransactionStatus(
+            @PathVariable Long boardId,
+            @RequestParam State newState)
+    {
         boardService.changePostStatus(boardId, newState);
-        return ResponseEntity.ok("Transaction status changed successfully.");
+        return ResponseEntity.ok().build();
     }
+
 }
