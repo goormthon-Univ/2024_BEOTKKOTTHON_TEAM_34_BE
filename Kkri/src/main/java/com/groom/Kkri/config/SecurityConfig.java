17ec0eeb7ac22db51f858f99332bb659746fd2c8
simjaeyoun <spqjfgogo@gmail.com>
2024-03-22 23:06:20 +0900

    feat : multipartfile받도록 설정

--- Kkri/src/main/java/com/groom/Kkri/config/SecurityConfig.java
+++ Kkri/src/main/java/com/groom/Kkri/config/SecurityConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.Collections;
@@ -27,6 +29,18 @@ public class SecurityConfig {
                     configuration.setMaxAge(3600L);
                     return configuration;
                 })));
+        http.
+                authorizeHttpRequests((auth) -> auth
+                        .anyRequest().permitAll()
+                );
+
+        http.
+                csrf(AbstractHttpConfigurer::disable);
+        http.
+                formLogin(AbstractHttpConfigurer::disable);
+        http.
+                httpBasic(AbstractHttpConfigurer::disable);
+
         return http.build();
     }
 }
