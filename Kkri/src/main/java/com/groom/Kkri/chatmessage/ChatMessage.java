a77953bd36110c9c45f9210513e49fec2abf08e1
simjaeyoun <spqjfgogo@gmail.com>
2024-03-23 15:51:54 +0900

    refactor : 코드 주석

--- Kkri/src/main/java/com/groom/Kkri/chatmessage/ChatMessage.java
+++ Kkri/src/main/java/com/groom/Kkri/chatmessage/ChatMessage.java
@@ -1,33 +1,33 @@
-package com.groom.Kkri.chatmessage;
-
-import com.groom.Kkri.entity.BaseTimeEntity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.bson.types.ObjectId;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.time.LocalDateTime;
-
-@Document
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
-public class ChatMessage {
-    @Id
-    private String id;
-    private String chatRoomId;
-    private String senderId;
-    private String recipientId;
-    private String content;
-
-    @CreatedDate
-    private LocalDateTime createdAt;
-}
+//package com.groom.Kkri.chatmessage;
+//
+//import com.groom.Kkri.entity.BaseTimeEntity;
+//import jakarta.persistence.EntityListeners;
+//import jakarta.persistence.Id;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import org.bson.types.ObjectId;
+//import org.springframework.data.annotation.CreatedDate;
+//import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+//import org.springframework.data.mongodb.core.mapping.Document;
+//
+//import java.time.LocalDateTime;
+//
+//@Document
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@EntityListeners(AuditingEntityListener.class)
+//public class ChatMessage {
+//    @Id
+//    private String id;
+//    private String chatRoomId;
+//    private String senderId;
+//    private String recipientId;
+//    private String content;
+//
+//    @CreatedDate
+//    private LocalDateTime createdAt;
+//}
