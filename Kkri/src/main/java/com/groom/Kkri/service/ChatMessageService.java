13b3df0a0cf3ef14eb60f305128a479f561d7adc
simjaeyoun <spqjfgogo@gmail.com>
2024-03-23 15:49:28 +0900

    feat : 메세지 서비스 작성

---
+++ Kkri/src/main/java/com/groom/Kkri/service/ChatMessageService.java
@@ -0,0 +1,67 @@
+package com.groom.Kkri.service;
+
+import com.groom.Kkri.dto.message.ChatMessageDto;
+import com.groom.Kkri.dto.message.ChatCreateDto;
+import com.groom.Kkri.dto.message.ChatMsgAndRoomDto;
+import com.groom.Kkri.entity.Board;
+import com.groom.Kkri.entity.ChatMessage;
+import com.groom.Kkri.entity.ChatRoom;
+import com.groom.Kkri.repository.BoardRepository;
+import com.groom.Kkri.repository.ChatMessageRepository;
+import com.groom.Kkri.repository.ChatRoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ChatMessageService {
+    private final ChatMessageRepository chatMessageRepository;
+    private final ChatRoomRepository chatRoomRepository;
+    private final ChatRoomService chatRoomService;
+
+    public List<ChatMessageDto> getMessage(Long chatRoomId){
+        return chatMessageRepository.findByChatRoomId(chatRoomId, Sort.by(Sort.Direction.ASC,"createdDate")).stream()
+                .map(ChatMessageDto::new)
+                .toList();
+    }
+
+    @Transactional
+    public ChatMessageDto createMessage(ChatCreateDto chatCreateDto){
+
+        Long chatRoomId = chatCreateDto.getChatRoomId();
+        ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId).get();
+
+        ChatMessage chatMessage = ChatMessage.builder()
+                .chatRoom(chatRoom)
+                .senderId(chatCreateDto.getSenderId())
+                .content(chatCreateDto.getMessage())
+                .build();
+
+        chatMessageRepository.save(chatMessage);
+
+        chatRoom.updateLatestMessage(chatCreateDto.getMessage());
+
+        return new ChatMessageDto(chatMessage);
+    }
+
+    @Transactional
+    public ChatMessageDto createRoomAndMessage(ChatMsgAndRoomDto chatMsgAndRoomDto){
+        ChatRoom room = chatRoomService.createRoom(chatMsgAndRoomDto);
+
+        ChatMessage chatMessage = ChatMessage.builder()
+                .chatRoom(room)
+                .senderId(chatMsgAndRoomDto.getSenderId())
+                .content(chatMsgAndRoomDto.getMessage())
+                .build();
+
+        chatMessageRepository.save(chatMessage);
+
+        return new ChatMessageDto(chatMessage);
+    }
+}
